2a6cf10bbea64411bdb54840a1d4f583b2a16346
arjunjoshua <arjunjoshua@gmail.com>
2023-06-09 13:17:27 +0100

    Implemented calls to Pokeapi to pull data.

--- src/components/Pokemon_grid.tsx
+++ src/components/Pokemon_grid.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import { pokemonData } from './data_Kanto';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { PokemonCard } from './Pokemon_card';
-import '../styles/styles.css'
+import { Pokemon } from './Pokemon_struct';
 
 export const PokemonGrid: React.FC = () => {
+  const [pokemonData, setPokemonData] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151');
+      const pokemonArray: Pokemon[] = response.data.results.map((pokemon: any, index: number) => {
+        return {
+          id: index + 1,
+          name: pokemon.name,
+          sprite: `https://img.pokemondb.net/sprites/black-white/normal/${pokemon.name}.png`,
+        }
+      });
+      setPokemonData(pokemonArray);
+    };
+
+    fetchPokemonData();
+  }, []);
+
   return (
     <div className="pokemon-grid">
       {pokemonData.map((pokemon) => (
