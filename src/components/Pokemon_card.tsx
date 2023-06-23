ae771d32a669f9adbad1a92c1fd4cd67d98f4c83
arjunjoshua <arjunjoshua@gmail.com>
2023-06-23 14:48:00 +0100

    Improved styling on the pokemon card

--- src/components/Pokemon_card.tsx
+++ src/components/Pokemon_card.tsx
@@ -15,12 +15,14 @@ export const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon, showShiny }) 
     <div className={`pokemon-card ${pokemon.isUnreleased || (pokemon.shinyUnreleased && showShiny) ? 'unreleased' : ''}`}>
       <img src={spriteToUse} alt={pokemon.name} />
       <p>{pokemon.name}</p>
-      {pokemon.types.map((typeObj, index) => (
-        <div key={index} className={`pokemon-type ${typeObj.name}`}>
-          {typeObj.name}
-    </div>
-      ))}
-    </div>
+      <div className="types">
+    {pokemon.types.map((type, index) => (
+      <div key={index} className={`type type-${type.name}`}>
+        {type.name}
+      </div>
+    ))}
+  </div>
+</div>
   );
 };
 
